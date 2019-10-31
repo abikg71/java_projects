32ed806db5aad0a17e36f20422aec1771e34b402
abikg71 <abinetkenore14@yahoo.com>
2019-10-30 20:50:13 -0600

    Dry Temperature.

--- HelpJava/HelpTemperatureDRY.java
+++ HelpJava/HelpTemperatureDRY.java
@@ -1,4 +1,3 @@
-
 import java.util.*;
 /*
 @ Abinet Kenore
@@ -10,7 +9,7 @@ public class HelpTemperatureDRY
 {
   static double celsius;
   static double fahrenheit;
-  
+
   static double Fahrenheit;
   static double Celsius;
 
@@ -21,29 +20,26 @@ public class HelpTemperatureDRY
   }
   public static double FahrenheitTocelsius()
   {
-    Celsius =  (fahrenheit - 32) / 1.8;
-    return  Celsius;
+    Celsius = (fahrenheit - 32) / 1.8;
+    return Celsius;
   }
-
   public static void main(String[] args)
   {
     Scanner input = new Scanner(System.in);
     boolean ch = false;
-    while(!ch )
+    while (!ch)
     {
       System.out.println("This a part Help... continued");
       int choose = 0;
-      System.out.println("Please choose 1 for C -> F and 2 for " +
-                    "F -> C: ");
+      System.out.println("Please choose 1 for C -> F and 2 for " + "F -> C: ");
       choose = input.nextInt();
       switch (choose)
       {
         case 1:
         System.out.println("Enter the Celsius");
-        celsius= input.nextDouble();
-        System.out.println( "Temperature in Fahrenheit is: "       +celsiusToFahrenheit());
+        celsius = input.nextDouble();
+        System.out.println("Temperature in Fahrenheit is: " + celsiusToFahrenheit());
         break;
-
         case 2:
         System.out.println("Enter the fahrenheit ");
         fahrenheit = input.nextDouble();
@@ -52,23 +48,23 @@ public class HelpTemperatureDRY
 
         default:
         break;
-      }
-      System.out.println("Another calculation?\n  " +
-                        "y / n, press y to continue or any "
-                        + "other key to exit ");
-      String YN;
-      input.nextLine();
-      YN = input.nextLine();
-      if(YN.equalsIgnoreCase("y"))
-      {
-        ch = false;
-      }
+       }
 
-      else
-      {
-        ch = true;
-        System.out.println("You exited(!).");
-      }
+    System.out.println("Another calculation?\n  " + "y / n, press y to continue or any  other key to exit ");
+
+    String YN;
+    input.nextLine();
+    YN = input.nextLine();
+    if (YN.equalsIgnoreCase("y"))
+    {
+      ch = false;
     }
+    else
+    {
+      ch = true;
+      System.out.println("You exited(!).");
+    }
+
   }
+ }
 } // End of class
